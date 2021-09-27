99a106e22fce4d7c216f73c11aee21b26b209484
genghiskhang <khang11475@gmail.com>
2021-09-27 17:35:09 -0400

    imported modules

---
+++ dashboard.py
@@ -0,0 +1,10 @@
+#Pandas, Plotly, and NumPy modules
+import pandas as pd
+import plotly
+import numpy as np
+
+#Modules imported to retrieve data from Google Sheets
+import gspread
+from df2gspread import df2gspread as d2g
+from oauth2client.service_account import ServiceAccountCredentials
+
