eb4d19a4342624525c2631ec75b3d900a00d7fcd
genghiskhang <khang11475@gmail.com>
2021-09-28 10:28:35 -0400

    sample data scatterplot

--- dashboard.py
+++ dashboard.py
@@ -1,10 +1,15 @@
 #Pandas, Plotly, and NumPy modules
 import pandas as pd
-import plotly
 import numpy as np
+import plotly.express as px
 
 #Modules imported to retrieve data from Google Sheets
 import gspread
 from df2gspread import df2gspread as d2g
 from oauth2client.service_account import ServiceAccountCredentials
 
+xVals = np.random.randn(20).tolist()
+yVals = np.random.randn(20).tolist()
+
+fig = px.scatter(x=xVals, y=yVals, width=1)
+fig.show()
